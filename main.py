834aaef4b9f5afbc8e8b5aab4bb3300573a16ea0
bigtony-256 <137121150+bigtony-256@users.noreply.github.com>
2024-02-12 15:11:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-//from js import document
-//from pyodide import create_proxy
+#from js import document
+#from pyodide import create_proxy
 
-//function_proxy = create_proxy(runPython)
+#function_proxy = create_proxy(runPython)
 
 print ("hello")
